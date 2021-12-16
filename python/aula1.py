01a0f4f9fd6c3f3b5b994e5946b90a8d5fb17721
/matheus <matheus.nsantos@bandtec.com.br>
2021-12-16 19:15:11 -0300

    Conteúdo da primeira aula

---
+++ python/aula1.py
@@ -0,0 +1,15 @@
+#Criando variáveis com vários tipos e inputs (String, float e int)
+nome = input("Digite um funcionário: ") 
+empresa = input("Digite a instituição: ")
+qtdFuncionarios = int(input("Digite a qtde de funcionários: ")) #Int... um valor inteiro
+mediaMensalidade = float(input("Digite a média da mensalidade: "))  #Float... um valor decimal
+#Aqui será exibido as variáveis unidas em uma única string
+print(nome + " trabalha na empresa " + empresa)
+print("Possui: ", qtdFuncionarios, " funcionarios.")
+print("A média da mensalidade é de: " + str(mediaMensalidade))
+print("============== Verifique os tipos de dados abaixo: ==============")
+#Exibindo o tipo das variáveis
+print("O tipo de dado da variavel [nome] é: ",type(nome))
+print("O tipo de dado da variavel [empresa] é: ",type(empresa))
+print("O tipo de dado da variavel [qtdFuncionarios] é: ",type(qtdFuncionarios))
+print("O tipo de dado da variavel [mediaMensalidade] é: ",type(mediaMensalidade))
